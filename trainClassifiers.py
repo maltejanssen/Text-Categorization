1ccdd559f8ea946ff5d01e720c59ec5563588b96
maltejanssen <malte.janssen@haw-hamburg.de>
2019-04-13 16:43:17 +0200

    close #7 put code into script

--- trainClassifiers.py
+++ trainClassifiers.py
@@ -1,6 +1,7 @@
 import nltk
 import math
 import pandas
+import argparse
 
 
 def getWords(categorizedCorpus, category):
@@ -15,43 +16,80 @@ def splitData(data, fraction):
     return data[0:cutoff], data[cutoff:]
 
 
+def loadData(data, fraction):
+    trainData = {}
+    testData = {}
+
+    if data == "BBC":
+        data = pandas.read_csv("bbc-text.csv")
+        dataDict = {}
+
+        for index, row in data.iterrows():
+            if row["category"] not in dataDict:
+                dataDict[row["category"]] = []
+            dataDict[row["category"]].append(nltk.word_tokenize(row["text"]))
+
+        for label in dataDict:
+            trainData[label], testData[label] = splitData(datadict[label], fraction)
+
+    elif data == "SFU":
+        corpus = nltk.corpus.reader.CategorizedPlaintextCorpusReader("corpus", ".*", cat_pattern=r'(\w+)/*', encoding='latin-1')
+        trainData = {}
+        testData = {}
+        labels = corpus.categories()
+
+        for label in labels:
+            data = getWords(corpus, label)
+            instances = [i for i in data if i]
+            trainData[label], testData[label] = splitData(instances, fraction)
+    else:
+        raise ValueError("Data not supported")
+
+    return trainData, tetsData
+
+
+def train(args):
+    data = loadData(args.corpus, args.fraction)
 
 
 
+def addArguments():
+    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description='Script that trains text-Classifiers')
+    parser.add_argument("--corpus", default=r"Data\Corpus", help="Options: BBC, SFU")
+    parser.add_argument("--fraction", default=0.7, help="split-fraction of data into train and test dataset")
+    parser.add_argument("--classifier", default="all", help="Classifier to be used; Options:")  # TODO options
+    parser.add_argument("--eval", action='store_true', default=True, help="do evaluation")
+    parser.add_argument("--backoff", default="True", help="turn on/off backoff functionality for n-grams")
 
-#SFU Data
-corpus = nltk.corpus.reader.CategorizedPlaintextCorpusReader("corpus", ".*", cat_pattern=r'(\w+)/*', encoding='latin-1')
-trainData = {}
-testData = {}
-labels = corpus.categories()
+    maxentGroup = parser.add_argument_group("Maxent Classifier")
+    maxentGroup.add_argument("-maxIter", default=10, type=int,
+                             help="Terminate after default: %(default)d iterations.")
+    maxentGroup.add_argument("--minll", default=0, type=float,
+                             help="Terminate after the negative average log-likelihood drops under default: %(default)f")
+    maxentGroup.add_argument("--minlldelta", default=0.1, type=float,
+                             help="Terminate if a single iteration improvesnlog likelihood by less than default, default is %(default)f")
 
-for label in labels:
-    data = getWords(corpus, label)
-    instances = [i for i in data if i]
-    trainData[label], testData[label] = splitData(instances, 0.7)
+    decisiontreeGroup = parser.add_argument_group("Decision Tree Classifier")
+    decisiontreeGroup.add_argument("--entropyCutoff", default=0.05, type=float,
+                                   help="default: 0.05")
+    decisiontreeGroup.add_argument("--depthCutoff", default=100, type=int,
+                                   help="default: 100")
+    decisiontreeGroup.add_argument("--supportCutoff", default=10, type=int,
+                                   help="default: 10")
 
-
-
-#BBC data
-data = pandas.read_csv("bbc-text.csv")
-dataDict = {}
-
-
-
-for index, row in data.iterrows():
-    if row["category"] not in dataDict:
-        dataDict[row["category"]] = []
-    dataDict[row["category"]].append([nltk.word_tokenize(row["text"])])
+    return parser.parse_args()
 
 
 
-print(dataDict)
-# trainDataBBC = {}
-# testDataBBC = {}
-#
-#
-#
-#
-# trainDataBBC, testDataBBC = splitData(data, 0.7)
-# print(trainDataBBC)
+if  __name__ == '__stupid__':
+    args = addArguments()
+
+    if args.classifier == "all":
+        for classifier in classifierOtions:
+            args.classifier = classifier
+            train(args)
+    else:
+        train(args)
+
 
